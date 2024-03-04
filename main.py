d6d8db58e7ccad2e9245b4719b942eb5db033d32
Anushka-N12 <anushka.narsima@gmail.com>
2024-03-04 12:30:57 +0400

    commiting first time

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, world!"
+
+
+if __name__ == '__main__':
+    app.run(port=os.getenv("PORT", default=5000))
